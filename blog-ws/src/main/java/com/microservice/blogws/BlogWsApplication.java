6f2e47f73213365fe2b73375efc4e590818905c5
Achal.Gupta <achal.gupta@solera.com>
2023-09-27 14:23:53 +0530

    added new web service

---
+++ blog-ws/src/main/java/com/microservice/blogws/BlogWsApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.blogws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogWsApplication.class, args);
+	}
+
+}
---
+++ blog-ws/src/test/java/com/microservice/blogws/BlogWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.blogws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
